8c650b5034b443fdce1166c75ba18bd83ca12c33
limoclaudiokrop <limoclaudiokrop@gmail.com>
2023-12-14 12:04:14 +0300

    Initial commit
    First commit

---
+++ Machine Learning.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Melbourne Housing Data
+# 
+# ## Decision Tree Regressor
+# 
+# ### Load and Examine Data
+# The data is loaded into train_X, val_X, train_y and val_y
+
+# In[10]:
+
+
+# Data Loading Code Runs At This Point
+import pandas as pd
+    
+# Load data
+melbourne_file_path = './melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing values
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and features
+y = filtered_melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_features]
+
+X.describe()
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+
+
+# We can use a utility function to help compare MAE scores from different values for max_leaf_nodes:
+
+# In[4]:
+
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+
+# In[5]:
+
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+
+# ## Random Forest Regression Model
+
+# In[11]:
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
+
+
+# ### Conclusion
+# There is likely room for further improvement, but this is a big improvement over the best decision tree error of 250,000. There are parameters which allow you to change the performance of the Random Forest much as we changed the maximum depth of the single decision tree. But one of the best features of Random Forest models is that they generally work reasonably even without this tuning.
+
+# ## Random Forest with Ordinal Encoding of Categorical Variable
+# Define Function to Measure Quality of Approach
+
+# In[12]:
+
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+
+# ### Ordinal Encoding
+# Scikit-learn has a OrdinalEncoder class that can be used to get ordinal encodings. We loop over the categorical variables and apply the ordinal encoder separately to each column.
+
+# In[20]:
+
+
+from sklearn.model_selection import train_test_split
+
+# Read the data
+data = pd.read_csv('./melb_data.csv')
+
+# Separate target from predictors
+y = data.Price
+X = data.drop(['Price'], axis=1)
+
+# Divide data into training and validation subsets
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                random_state=0
+                                                               )
+
+
+# Drop columns with missing values (simplest approach)
+cols_with_missing = [col for col in X_train_full.columns if X_train_full[col].isnull().any()] 
+X_train_full.drop(cols_with_missing, axis=1, inplace=True)
+X_valid_full.drop(cols_with_missing, axis=1, inplace=True)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
+                        X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = low_cardinality_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+
+# In[21]:
+
+
+# Get list of categorical variables
+s = (X_train.dtypes == 'object')
+object_cols = list(s[s].index)
+
+print("Categorical variables:")
+print(object_cols)
+
+
+# In[22]:
+
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+
+# In[23]:
+
+
+from sklearn.preprocessing import OrdinalEncoder
+
+# Make copy to avoid changing original data 
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply ordinal encoder to each column with categorical data
+ordinal_encoder = OrdinalEncoder()
+label_X_train[object_cols] = ordinal_encoder.fit_transform(X_train[object_cols])
+label_X_valid[object_cols] = ordinal_encoder.transform(X_valid[object_cols])
+
+print("MAE from Approach 2 (Ordinal Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+
+# ### Conclusion
+# It is apparent that using Ordinal encoding for categorical variable Region name, and setting the number of n_estimators for Random Forest Regression has significantly improved the performance of the model from 191669 to 165936.
+
+# # XGBoost
+#  XGBoost stands for extreme gradient boosting, which is an implementation of gradient boosting with several additional features focused on performance and speed.
+
+# In[3]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+data = pd.read_csv('./melb_data.csv')
+
+# Select subset of predictors
+cols_to_use = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
+X = data[cols_to_use]
+
+# Select target
+y = data.Price
+
+# Separate data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+
+# In[4]:
+
+
+from xgboost import XGBRegressor
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05, n_jobs=4)
+my_model.fit(X_train, y_train, 
+             early_stopping_rounds=5, 
+             eval_set=[(X_valid, y_valid)], 
+             verbose=False)
+
+
+# In[5]:
+
+
+from sklearn.metrics import mean_absolute_error
+
+predictions = my_model.predict(X_valid)
+print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
+
+
+# In[ ]:
+
+
+
+
